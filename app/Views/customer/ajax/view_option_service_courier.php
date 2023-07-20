d140d53ae04d8c14758970ac739f2e75bbd33924
Mohamad Fayyaz <fayyazmuhammad693@gmail.com>
2023-07-20 14:06:14 +0700

    revisi

--- app/Views/customer/ajax/view_option_service_courier.php
+++ app/Views/customer/ajax/view_option_service_courier.php
@@ -4,7 +4,7 @@
     <?php foreach ($costs as $value) : ?>
         <div class="form-check form-check-inline w-25">
             <input type="hidden" name="service" value="<?= $value['service'] ?>">
-            <input class="form-check-input" type="radio" name="cost" onclick="addCourier(<?= $value['cost'][0]['value'] ?>)" id="<?= $value['service'] ?>" value="<?= $value['cost'][0]['value'] ?>">
+            <input class="form-check-input" type="radio" name="cost" onclick="addCourier(<?= $value['cost'][0]['value'] ?>)" id="<?= $value['service'] ?>" value="<?= $value['cost'][0]['value'] ?>" required>
             <label class="form-check-label" for="<?= $value['service']  ?>"><?= $value['service'] ?></label>
             <label class="form-check-label" for="<?= $value['service']  ?>">Rp. <?= number_format($value['cost'][0]['value'], 0, ",", "."); ?> - <?= str_contains($value['cost'][0]['etd'], 'HARI') ?  $value['cost'][0]['etd'] : $value['cost'][0]['etd'] . ' HARI' ?> </label>
         </div>
